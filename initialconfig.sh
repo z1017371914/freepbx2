35c76f045ddce853cc111ef0cad2fe752683a228
snsyn <alexmartinmerino@gmail.com>
2016-06-01 20:43:49 +0200

    cambio minimo

--- initialconfig.sh
+++ initialconfig.sh
@@ -2,4 +2,4 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo -i
 sudo -E apt-get -y install mysql-server
-sudo apt-get install openssh-server mysql-client
+sudo apt-get -y install openssh-server mysql-client
