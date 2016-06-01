29683ab403d3311bdaf8d23fdc84719d8f2b2276
snsyn <alexmartinmerino@gmail.com>
2016-06-01 23:37:44 +0200

    mas cambios

--- initialconfig.sh
+++ initialconfig.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sudo apt-get update && apt-get upgrade -y
+DEBIAN_FRONTEND=noninteractive sudo apt-get install mysql-server
 sudo apt-get install -y build-essential linux-headers-`uname -r` openssh-server mysql-client apache2\
-  bison flex php5 php5-curl php5-cli php5-mysql php-pear php5-gd curl sox mysql-server\
+  bison flex php5 php5-curl php5-cli php5-mysql php-pear php5-gd curl sox\
   libncurses5-dev libssl-dev libmysqlclient-dev mpg123 libxml2-dev libnewt-dev sqlite3\
   libsqlite3-dev pkg-config automake libtool autoconf git unixodbc-dev uuid uuid-dev\
   libasound2-dev libogg-dev libvorbis-dev libcurl4-openssl-dev libical-dev libneon27-dev libsrtp0-dev\
